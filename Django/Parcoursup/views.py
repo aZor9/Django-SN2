4eb4de861b265d76c6441ca08eb10d7a8465e2c3
Hugo <hugo.lembrez@gmail.com>
2024-10-10 12:57:35 +0200

    login

--- Django/Parcoursup/views.py
+++ Django/Parcoursup/views.py
@@ -5,3 +5,25 @@ from django.http import HttpResponse
 
 def hello_world(request):
     return HttpResponse("Hello, World et Bonjour")
+
+
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Connexion réussie !")
+            return redirect('home')  # Remplacez 'home' par le nom de votre page d'accueil
+        else:
+            messages.error(request, "Nom d'utilisateur ou mot de passe incorrect.")
+    
+    return render(request, 'login.html')
