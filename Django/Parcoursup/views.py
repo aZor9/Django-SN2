53b49e48920ac93bfbe777c5cf561678f5d256e5
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2024-10-10 14:02:36 +0200

    index

--- Django/Parcoursup/views.py
+++ Django/Parcoursup/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def hello_world(request):
-    return HttpResponse("Hello, World et Bonjour")
+def index (request):
+   return render(request, 'index.html')
