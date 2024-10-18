d294077baf268fdb5a4a504901491eb9a2fa7362
Hugo <hugo.lembrez@gmail.com>
2024-10-18 10:53:52 +0200

    pause du register student

--- Django/Parcoursup/forms.py
+++ Django/Parcoursup/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from .models import OfferPro  # Modifiez le nom ici
-from .models import Student
+# from .models import Student
 
 class OfferProForm(forms.ModelForm):  # Changez également le nom de la classe
     class Meta:
         model = OfferPro  # Utilisez le modèle OfferPro
         fields = ['title', 'description', 'image_url']  # Listez les champs que vous souhaitez dans le formulaire
 
-class student_form(forms.ModelForm):
-    class Meta :
-        model = Student
-        fields = ('firstname', 'email')
+# class student_form(forms.ModelForm):
+#     class Meta :
+#         model = Student
+#         fields = ('firstname', 'email')
