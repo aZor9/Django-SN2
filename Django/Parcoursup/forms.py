efd7296cbd6c38d8577b33157908e47573d1f9b9
Hugo <hugo.lembrez@gmail.com>
2024-10-23 16:54:23 +0200

    sans offre pro

--- Django/Parcoursup/forms.py
+++ Django/Parcoursup/forms.py
@@ -1,14 +1,10 @@
 from django import forms
-from .models import OfferPro  
 from .models import Student
 from .models import Etablissement
 from django.contrib.auth.models import User
 
 
-class OfferProForm(forms.ModelForm):  # Changez également le nom de la classe
-    class Meta:
-        model = OfferPro  # Utilisez le modèle OfferPro
-        fields = ['title', 'description', 'image_url']  # Listez les champs que vous souhaitez dans le formulaire
+
 
 class student_form(forms.ModelForm):
     # Champs provenant du modèle User
