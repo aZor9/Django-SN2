fcf965b0ce740b023ca6061e1b24a82c32ebf743
Hugo <hugo.lembrez@gmail.com>
2024-10-23 15:52:45 +0200

    BDD etablissement

--- Django/Parcoursup/forms.py
+++ Django/Parcoursup/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import OfferPro  
 from .models import Student
+from .models import Etablissement
+from django.contrib.auth.models import User
 
 
 class OfferProForm(forms.ModelForm):  # Changez également le nom de la classe
@@ -26,10 +28,43 @@ class student_form(forms.ModelForm):
             email=self.cleaned_data['email']
         )
         
-        student = super(StudentForm, self).save(commit=False)
+        student = super(student_form, self).save(commit=False)
         student.user = user  # Associe l'utilisateur créé avec l'étudiant
         
         if commit:
             user.save()
             student.save()
         return student
+
+
+
+
+
+
+
+class Etablissement_form(forms.ModelForm):
+    # Champs provenant du modèle User
+    username = forms.CharField(max_length=150, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = Etablissement
+        fields = ['username', 'adress', 'city', 'country']  # Champs du modèle Etablissement     plus demandé : 'high_school'
+
+    def save(self, commit=True):
+        # Sauvegarde des informations User et Etablissement
+        user = User.objects.create_user(
+            username=self.cleaned_data['username'],
+            password=self.cleaned_data['password'],
+            email=self.cleaned_data['email']
+        )
+        
+
+        Etablissement = super(Etablissement_form, self).save(commit=False)
+        Etablissement.user = user  # Associe l'utilisateur créé avec l'étudiant
+        
+        if commit:
+            user.save()
+            Etablissement.save()
+        return Etablissement
