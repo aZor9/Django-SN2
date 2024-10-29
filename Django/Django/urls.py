298d410cb38695ea175f8f0e855be3b6f207adbf
Hugo <hugo.lembrez@gmail.com>
2024-10-29 12:06:45 +0100

    commentaire suite

--- Django/Django/urls.py
+++ Django/Django/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Parcoursup.urls')),
+    path('admin/', admin.site.urls), # page admin gerer par Django
+    path('', include('Parcoursup.urls')), # page d'accueil
 ]
