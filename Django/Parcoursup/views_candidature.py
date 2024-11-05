041e0d82b51c293271bb12b7a4c3bdf66132d23a
aZor9 <hugo.lembrez@gmail.com>
2024-11-05 20:36:32 +0100

    postulation

--- Django/Parcoursup/views_candidature.py
+++ Django/Parcoursup/views_candidature.py
@@ -7,6 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .models import *
 from .forms import *
+from django.utils import timezone
 
 
 
@@ -16,17 +17,31 @@ def candidature(request):
 
 @login_required
 def postuler(request, offer_id):
-    offer = get_object_or_404(Offer, id=offer_id)
-    student_profile = request.user.userprofile
+    # Récupérer l'offre à partir de l'ID
+    offre = get_object_or_404(Offer, id=offer_id)
 
-    # Vérifie si l'utilisateur est un étudiant et n'a pas déjà postulé
-    if student_profile.user_type == 'student':
-        if not Application.objects.filter(student=student_profile, offer=offer).exists():
-            Application.objects.create(student=student_profile, offer=offer, status='pending')
-            messages.success(request, 'Votre candidature a été soumise avec succès.')
-        else:
-            messages.info(request, 'Vous avez déjà postulé pour cette offre.')
-    else:
-        messages.error(request, "Seuls les étudiants peuvent postuler à cette offre.")
+    # Récupérer le profil utilisateur
+    user_profile = request.user.userprofile
 
-    return redirect('offre_id', offer_id=offer.id)
+    # Vérifier si l'utilisateur a déjà postulé pour cette offre
+    if Application.objects.filter(student=user_profile, offer=offre).exists():
+        messages.warning(request, "Vous avez déjà postulé pour cette offre.")
+        return redirect('offre_id', offer_id=offer_id)
+
+    # Si la méthode de requête est POST, créer une nouvelle candidature
+    if request.method == "POST":
+        # Créer une nouvelle instance d'Application
+        application = Application(
+            student=user_profile,
+            offer=offre,
+            application_date=timezone.now(),  # Date actuelle de la candidature
+            status='pending'  # Statut par défaut
+        )
+        application.save()  # Enregistrer l'objet dans la base de données
+
+        # Afficher un message de succès
+        messages.success(request, "Votre candidature a été soumise avec succès.")
+        return redirect('offre_id', offer_id=offer_id)
+
+    # Si ce n'est pas une méthode POST, redirigez vers l'offre
+    return render(request, 'offre.html', {'offre': offre})
